f9273c9028d8e426847b2f9308fa7782000fd4ed
Travis Saucedo <travissaucedo2514@gmail.com>
2021-08-17 22:25:59 -0500

    HTML and CSS finished, Javascript still needs a lot of work

---
+++ script.js
@@ -0,0 +1,190 @@
+var questions = [
+  {
+    question: "How do you link your javascript in HTML?",
+    choice1: "`<link>`",
+    choice2: "`<span>`",
+    choice3: "`<script>`",
+    choice4: "`<main>`",
+    correctChoice: "choice3",
+  },
+  {
+    question: `What is the correct way to call a class in CSS?(class="fancy")`,
+    choice1: "#fancy",
+    choice2: ".fancy",
+    choice3: "fancy",
+    choice4: "fancy();",
+    correctChoice: "choice2",
+  },
+  {
+    question: "Where does <script> go in HTML",
+    choice1: "In <head>",
+    choice2: "Outside the <html>",
+    choice3: "At the bottom of <body>",
+    choice4: "None of these",
+    correctChoice: "choice3",
+  },
+  {
+    question: "What is the correct way to link your Javascript?",
+    choice1: `"<script href="script.js">"`,
+    choice2: `"<script link="script.js">"`,
+    choice3: `"<script source="script.js">"`,
+    choice4: `"<script src="script.js">"`,
+    correctChoice: "choice4",
+  },
+  {
+    question: "What is the correct way to link your CSS?",
+    choice1: `"<link href="style.css">"`,
+    choice2: `"<link src="style.css">"`,
+    choice3: `"<link source="style.css">"`,
+    choice4: `"<link rel="style.css">"`,
+    correctChoice: "choice1",
+  },
+  {
+    question: "What is the correct sytanx for creating a function?",
+    choice1: `function = "#"`,
+    choice2: "function() {}",
+    choice3: "function:myFunction()",
+    choice4: "var function = myFunction()",
+    correctChoice: "choice2",
+  },
+  {
+    question: "What is the correct way to call a fucntion?",
+    choice1: "myFucntion()",
+    choice2: "myFucntion = call",
+    choice3: "myFunction[]",
+    choice4: "call.myFunction()",
+    correctChoice: "choice1",
+  },
+  {
+    question: `What is the definiton of "this"?`,
+    choice1:
+      "Used to identify a specific person or thing close at hand or being indicated or experienced",
+    choice2: `The "this" keyword refers to the object from which the function was called`,
+    choice3: "Referring to a specific thing or situation just mentioned",
+    choice4: "To the degree or extent indicated",
+    correctChoice: "choice2",
+  },
+  {
+    question: "What is the correct way to comment out something?",
+    choice1: "// Comment",
+    choice2: "/* Comment */",
+    choice3: "<-- Comment -->",
+    choice4: "These are all correct",
+    correctChoice: "choice4",
+  },
+  {
+    question: "What is the correct syntax for writing out an array?",
+    choice1: `var choices = ["1", "2", "3", "4"]`,
+    choice2: `var choices = {"1 2 3 4"}`,
+    choice3: `var choices = "1 2 3 4"`,
+    choice4: `var choices = ("1", "2", "3", "4")`,
+    correctChoice: "choice1",
+  },
+];
+
+var choices = document.getElementsByName("choice");
+var questionNumber = 1;
+var currentScore = 0;
+var wrongAttempt = 0;
+var indexNumber = 0;
+
+var questionShuffle = [];
+
+function questionShuffler() {
+  while (questionShuffle.length <= 9) {
+    var random = questions[Math.floor(Math.random() * questions.length)];
+    if (!questionShuffle.includes(random)) {
+      questionShuffle.push(random);
+    }
+  }
+}
+
+function nextQuestion(input) {
+  questionShuffler();
+
+  var currentQuestion = questionShuffle[input];
+  document.querySelector(".question-number").innerHTML = questionNumber;
+  document.querySelector(".current-score").innerHTML = currentScore;
+  document.querySelector(".question-display").innerHTML =
+    currentQuestion.question;
+  document.getElementById("choice-one-label").innerHTML =
+    currentQuestion.choice1;
+  document.getElementById("choice-two-label").innerHTML =
+    currentQuestion.choice2;
+  document.getElementById("choice-three-label").innerHTML =
+    currentQuestion.choice3;
+  document.getElementById("choice-four-label").innerHTML =
+    currentQuestion.choice4;
+}
+
+function checkAnswer() {
+  var currentQuestion = questionShuffle[indeNumber];
+  var currentQuestionAnswer = currentQuestion.correctChoice;
+  var correctChoice = null;
+
+  choices.forEach((choice) => {
+    if (choice.value === currentQuestionAnswer) {
+      correctChoice = choice.labels[0].id;
+    }
+  });
+
+  if (
+    choices[0].checked === false &&
+    choices[1].checked === false &&
+    choices[2].checked === false &&
+    choices[3].checked === false
+  ) {
+    document.getElementById("choices-con").style.display = "flex";
+  }
+
+  choices.forEach((choice) => {
+    if (choice.checked === true && choice.value === currentQuestionAnswer) {
+      document.getElementById(correctChoice).style.background = "green";
+
+      currentScore++;
+      indexNumber++;
+
+      setTimeout(() => {
+        questionNumber++;
+      }, 1000);
+    } else if (choice.checked && choice.value !== currentQuestionAnswer) {
+      var wrongId = choice.labels[0].id;
+
+      document.getElementById(wrongId).style.background = "red";
+      document.getElementById(correctChoice).style.background = "green";
+
+      wrongAttempt++;
+      indexNumber++;
+
+      setTimeout(() => {
+        questionNumber++;
+      }, 1000);
+    }
+  });
+}
+
+function handleNextQuestion() {
+  checkAnswer();
+  clearChecks();
+
+  setTimeout(() => {
+    if (indexNumber <= 9) {
+      nextQuestion(indexnumber);
+    } else {
+      handleEndGame();
+    }
+    resetBackground();
+  }, 1000);
+}
+
+function resetBackground() {
+  choices.forEach((choice) => {
+    document.getElementById(choice.labels[0].id).style.background = "";
+  });
+}
+
+function clearChecks() {
+  for (var i = 0; i < choices.length; i++) {
+    choices[i].checked = false;
+  }
+}
